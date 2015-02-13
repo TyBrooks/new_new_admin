a981755a35f0cc2ce7770b052c852a9d4710b888
Ty Brooks <ty@viglink.com>
2015-02-13 11:59:05 -0800

    Get sass support working with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,27 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var replace = require('gulp-replace');
+
+var paths = {
+  css: "app/stylesheets/compiled/",
+  sass: "app/stylesheets/scss/",
+  bower: "app/bower_components/"
+}
+
+var bases = {
+  app: "app/"
+}
+
+gulp.task('default', function() {
+  
+})
+
+gulp.task('watch', function() {
+  gulp.watch(paths.scss + '*.scss', ['sass']);
+})
+
+gulp.task('sass', function() {
+  gulp.src(paths.sass + 'app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(paths.css))
+})

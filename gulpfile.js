57f4eb29b2a79fef34c2e285d1d9dfc14a5db352
Ty Brooks <ty@viglink.com>
2015-02-13 12:32:00 -0800

    Write regexs for renaming

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var replace = require('gulp-replace');
+var argv = require('yargs').argv;
 
 var paths = {
   css: "app/stylesheets/compiled/",
@@ -24,4 +25,25 @@ gulp.task('sass', function() {
   gulp.src(paths.sass + 'app.scss')
     .pipe(sass())
     .pipe(gulp.dest(paths.css))
+})
+
+
+gulp.task('rename-app', function() {
+  if ( argv.old && argv.new ) {
+    var regexs = [
+      new RegExp("(module\\(\\s*)" + argv.old + "((?:\\..+)?\\s*\\))", "g"),
+      new RegExp("(" + "describe\\(" + "\\s*" + ")" + argv.old + "(" + "(?:\\..+)?" + "\\s*" + "\\)" + ")", "g"),
+      new RegExp("(" + "ng\\-app\\=" + "(?:\\'|\\\")" + "\\s*" + ")" + argv.old + "(" + "\\s+" + "(?:\\'|\\\")" + ")", "g")
+    ]
+    
+    var replacement = "$1" + argv.new + "$2";
+    
+    gulp.src([
+      bases.app + '**',
+      "!" + paths.bower_components
+    ]).pipe( replace( regexs[0], replacement) )
+      .pipe( replace( regexs[1], replacement) )
+      .pipe( replace( regexs[2], replacement) )
+      .pipe( gulp.dest('./'))
+  }
 })

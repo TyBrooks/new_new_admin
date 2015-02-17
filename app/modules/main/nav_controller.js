05a6149f11a0d34480e706893f5eec8a12ad0b9d
Ty Brooks <ty@viglink.com>
2015-02-17 14:43:01 -0800

    Rename nav module folder to main. This is where the index/layout template will live

---
+++ app/modules/main/nav_controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('adminApp.nav', [])
+
+.controller('NavCtrl', ['$state', 'auth', function($state, auth) {
+
+  var init = function(){
+    // console.log($state);
+  };
+  
+  init();
+}]);
--- app/modules/nav/nav_controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('adminApp.nav', [])
-
-.controller('NavCtrl', [function() {
-
-}]);

d6156d8a971b485aeff63717621b6e122f606c8b
Ty Brooks <ty@viglink.com>
2015-02-20 16:45:48 -0800

    Add messages for item upload failure; add console logging to help debug issues

--- app/modules/custom_insert/custom_insert_controller.js
+++ app/modules/custom_insert/custom_insert_controller.js
@@ -1,10 +1,29 @@
 angular.module('adminApp.customInsert', [])
 
 .controller('CustomInsertCtrl', ['$scope', 'FileUploader', '$timeout', function($scope, FileUploader, $timeout) {
+
+/*
+ * Upload Section
+ */
   
   $scope.message = null;
   //Options for type: alert, success, info
   $scope.messageType = null;
+  
+  $scope.uploader = new FileUploader({
+    filters:[{
+      name: "CSV-only",
+      fn: function(item) {
+        var isCSV = !!item.name.match(/^.*\.csv$/);
+        return isCSV;
+      }
+    }],
+    removeAfterUpload: true
+  });
+  
+  $scope.params = {
+    type: "ae"
+  };
   
   $scope.displayMessage = function(msg, time, type) {
     $scope.message = msg;
@@ -17,27 +36,31 @@ angular.module('adminApp.customInsert', [])
     $scope.messageType = null;
   }
   
-  $scope.uploader = new FileUploader({
-    filters:[{
-      name: "CSV-only",
-      fn: function(item) {
-        var isCSV = !!item.name.match(/^.*\.csv$/);
-        if ( !isCSV ) {
-          $scope.displayMessage("Upload only supports CSV Files!", 4000, "alert");
-        }
-        return isCSV;
-      }
-    }],
-    removeAfterUpload: true
-  });
-  
-  $scope.params = {
-    type: "ae"
-  };
-  
   $scope.clearFile = function() {
     $scope.uploader.clearQueue();
   }
+  
+  $scope.setCallbacks = function( uploader ) {
+    uploader.onWhenAddingFileFailed = function( filter ) {
+      console.log("File adding failed. Failing filter: ", filter)
+      $scope.displayMessage("Upload only supports CSV Files!", 4000, "alert");
+    }
+    
+    uploader.onSuccessItem = function() {
+      $scope.displayMessage("Successfully Uploaded File. An email summary will be sent to you soon.", 4000, "success");
+    }
+    
+    uploader.onErrorItem = function( item, response, stat, headers ) {
+      console.log("File upload error. Debug info dump: item, response, status, headers", item, response, stat, headers)
+      $scope.displayMessage("File upload failed!", 6000, "alert");
+    }
+  }
+  
+  $scope.setCallbacks($scope.uploader);
+  
+/*
+ * Download section
+ */
   
   $scope.clearIds = function() {
     $scope.download.ids = [];

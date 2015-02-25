72b9aee00626a08d24c5cab29b3fe3986023e9d4
Ty Brooks <ty@viglink.com>
2015-02-24 18:06:57 -0800

    Add nav bar toggle

--- app/modules/main/nav_controller.js
+++ app/modules/main/nav_controller.js
@@ -4,6 +4,12 @@ angular.module('adminApp.nav', [])
 
 .controller('NavCtrl', ['$scope', 'auth', function($scope, auth) {
   $scope.user = { name: "" };
+  
+  $scope.showNav = true;
+  
+  $scope.toggleNav = function() {
+    $scope.showNav = !$scope.showNav;
+  }
 
   $scope.handleAuthFailure = function() {
     console.log("auth Failed");
